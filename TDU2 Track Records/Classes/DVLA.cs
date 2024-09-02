e7af77801b1b3db3f6d64d569f12ab4e481886ea
MoDuLah <modulah@gmail.com>
2024-09-02 23:10:07 +0100

    DB connection for vehicleCard

--- TDU2 Track Records/Classes/DVLA.cs
+++ TDU2 Track Records/Classes/DVLA.cs
@@ -1,4 +1,6 @@
-﻿namespace TDU2_Track_Records.Classes
+﻿using System;
+
+namespace TDU2_Track_Records.Classes
 {
     public class DVLA
     {
@@ -8,39 +10,39 @@
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Class { get; set; }
-        public int RacesRan { get; set; }
-        public double OdometerMetric { get; set; }
-        public double OdometerImperial { get; set; }
-        public bool Active { get; set; }
-        public bool Owned { get; set; }
+        public string RacesRan { get; set; }
+        public string OdometerMetric { get; set; }
+        public string OdometerImperial { get; set; }
+        public string Active { get; set; }
+        public string Owned { get; set; }
         public string Price { get; set; }
-        public int AccelerationStat { get; set; }
-        public int SpeedStat { get; set; }
-        public int BrakingStat { get; set; }
-        public int DifficultyStat { get; set; }
-        public int TopSpeed { get; set; }
-        public double AccelerationTime { get; set; }
+        public string AccelerationStat { get; set; }
+        public string SpeedStat { get; set; }
+        public string BrakingStat { get; set; }
+        public string DifficultyStat { get; set; }
+        public string TopSpeed { get; set; }
+        public string AccelerationTime { get; set; }
         public string Engine { get; set; }
         public string EngineLayout { get; set; }
         public string EnginePlacement { get; set; }
         public string Gearbox { get; set; }
         public string GearboxType { get; set; }
         public string GearboxMTAT { get; set; }
-        public int MaxTorque { get; set; }
-        public int MaxTorqueRPM { get; set; }
-        public int MaxPower { get; set; }
-        public int MaxPowerRPM { get; set; }
-        public int Weight { get; set; }
-        public double PowerWeightRatio { get; set; }
+        public string MaxTorque { get; set; }
+        public string MaxTorqueRPM { get; set; }
+        public string MaxPower { get; set; }
+        public string MaxPowerRPM { get; set; }
+        public string Weight { get; set; }
+        public string PowerWeightRatio { get; set; }
         public byte[] Image { get; set; } // For BLOB data
         public string Tag { get; set; }
         public string RegionTag { get; set; }
         public string InGarage { get; set; }
-        public int DealershipIdIbiza { get; set; }
-        public int DealershipIdHawaii { get; set; }
-        public bool CanVehiclePaint { get; set; }
-        public bool CanVehicleSticker { get; set; }
-        public bool CanVehicleUpgrade { get; set; }
+        public string DealershipIdIbiza { get; set; }
+        public string DealershipIdHawaii { get; set; }
+        public string CanVehiclePaint { get; set; }
+        public string CanVehicleSticker { get; set; }
+        public string CanVehicleUpgrade { get; set; }
         public string FrontTyres { get; set; }
         public string RearTyres { get; set; }
         public string WheelDrive { get; set; }
--- TDU2 Track Records/ViewModels/vehicleCardViewModel.cs
+++ TDU2 Track Records/ViewModels/vehicleCardViewModel.cs
@@ -1,65 +1,108 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SQLite;
 using TDU2_Track_Records.Classes;
+using TDU2_Track_Records.Properties;
 
 namespace TDU2_Track_Records.ViewModels
 {
-    internal class vehicleCardViewModel
+    internal class vehicleCardViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<DVLA> Vehicles { get; set; }
+        private readonly string connectionString;
 
         public vehicleCardViewModel()
         {
-            // Initialize with dummy data
-            Vehicles = new ObservableCollection<DVLA>
-            {
-                new DVLA
-                {
-                    Id = 1,
-                    Name = "Lamborghini Aventador",
-                    Brand = "Lamborghini",
-                    Model = "Aventador LP 780-4 Ultimae",
-                    Class = "S",
-                    RacesRan = 20,
-                    OdometerMetric = 15000.3,
-                    OdometerImperial = 9320.7,
-                    Active = true,
-                    Owned = true,
-                    Price = "$400,000",
-                    AccelerationStat = 92,
-                    SpeedStat = 98,
-                    BrakingStat = 89,
-                    DifficultyStat = 78,
-                    TopSpeed = 355,
-                    AccelerationTime = 2.8,
-                    Engine = "6.5L V12",
-                    EngineLayout = "Mid-engine",
-                    EnginePlacement = "Rear",
-                    Gearbox = "7-speed",
-                    GearboxType = "ISR",
-                    GearboxMTAT = "Automatic",
-                    MaxTorque = 720,
-                    MaxTorqueRPM = 5500,
-                    MaxPower = 780,
-                    MaxPowerRPM = 8500,
-                    Weight = 1550,
-                    PowerWeightRatio = 0.50,
-                    Image = null,
-                    Tag = "Supercar",
-                    RegionTag = "EU",
-                    InGarage = "Yes",
-                    DealershipIdIbiza = 2,
-                    DealershipIdHawaii = 4,
-                    CanVehiclePaint = true,
-                    CanVehicleSticker = true,
-                    CanVehicleUpgrade = true,
-                    FrontTyres = "255/30 ZR20",
-                    RearTyres = "355/25 ZR21",
-                    WheelDrive = "AWD",
-                    EnginePosition = "Rear",
-                    FrontBrakes = "Carbon-ceramic",
-                    RearBrakes = "Carbon-ceramic"
-                }
-            };
+            connectionString = Settings.Default.connectionString;
+            Vehicles = new ObservableCollection<DVLA>();
+            // Example: Load a vehicle with a specific ID
+            LoadVehicleById(192);
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoadVehicleById(int vehicleId)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM vehicles WHERE id = @id";
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", vehicleId);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var vehicle = new DVLA
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Brand = reader.GetString(reader.GetOrdinal("Brand")),
+                                Model = reader.GetString(reader.GetOrdinal("Model")),
+                                Class = reader.GetString(reader.GetOrdinal("Class")),
+                                RacesRan = reader.GetString(reader.GetOrdinal("Races_Ran")),
+                                OdometerMetric = reader.GetString(reader.GetOrdinal("Odometer_Metric")),
+                                OdometerImperial = reader.GetString(reader.GetOrdinal("Odometer_Imperial")),
+                                Active = reader.GetString(reader.GetOrdinal("Active")) == "1" ? "Yes" : "No",
+                                Owned = reader.GetString(reader.GetOrdinal("Owned")) == "1" ? "Yes" : "No",
+                                Price = reader.GetString(reader.GetOrdinal("Price")),
+                                AccelerationStat = reader.GetString(reader.GetOrdinal("Acceleration_Stat")),
+                                SpeedStat = reader.GetString(reader.GetOrdinal("Speed_Stat")),
+                                BrakingStat = reader.GetString(reader.GetOrdinal("Braking_Stat")),
+                                DifficultyStat = reader.GetString(reader.GetOrdinal("Difficulty_Stat")),
+                                TopSpeed = reader.GetString(reader.GetOrdinal("Top_Speed")),
+                                AccelerationTime = reader.GetString(reader.GetOrdinal("Acceleration_Time")),
+                                Engine = reader.IsDBNull(reader.GetOrdinal("Engine")) ? null : reader.GetString(reader.GetOrdinal("Engine")),
+                                EngineLayout = reader.IsDBNull(reader.GetOrdinal("Engine_Layout")) ? null : reader.GetString(reader.GetOrdinal("Engine_Layout")),
+                                EnginePlacement = reader.IsDBNull(reader.GetOrdinal("EnginePlacement")) ? null : reader.GetString(reader.GetOrdinal("EnginePlacement")),
+                                Gearbox = reader.IsDBNull(reader.GetOrdinal("Gearbox")) ? null : reader.GetString(reader.GetOrdinal("Gearbox")),
+                                GearboxType = reader.IsDBNull(reader.GetOrdinal("GearboxType")) ? null : reader.GetString(reader.GetOrdinal("GearboxType")),
+                                GearboxMTAT = reader.IsDBNull(reader.GetOrdinal("GearboxMTAT")) ? null : reader.GetString(reader.GetOrdinal("GearboxMTAT")),
+                                MaxTorque = reader.IsDBNull(reader.GetOrdinal("Max_Torque")) ? null : reader.GetString(reader.GetOrdinal("Max_Torque")),
+                                MaxTorqueRPM = reader.IsDBNull(reader.GetOrdinal("Max_TorqueRPM")) ? null : reader.GetString(reader.GetOrdinal("Max_TorqueRPM")),
+                                MaxPower = reader.IsDBNull(reader.GetOrdinal("Max_Power")) ? null : reader.GetString(reader.GetOrdinal("Max_Power")),
+                                MaxPowerRPM = reader.IsDBNull(reader.GetOrdinal("Max_PowerRPM")) ? null : reader.GetString(reader.GetOrdinal("Max_PowerRPM")),
+                                Weight = reader.IsDBNull(reader.GetOrdinal("Weight")) ? null : reader.GetString(reader.GetOrdinal("Weight")),
+                                PowerWeightRatio = reader.IsDBNull(reader.GetOrdinal("PowerWeightRatio")) ? null : reader.GetString(reader.GetOrdinal("PowerWeightRatio")),
+                                Image = reader["Image"] as byte[], // Handle BLOB data
+                                Tag = reader.IsDBNull(reader.GetOrdinal("Tag")) ? null : reader.GetString(reader.GetOrdinal("Tag")),
+                                RegionTag = reader.IsDBNull(reader.GetOrdinal("RegionTag")) ? null : reader.GetString(reader.GetOrdinal("RegionTag")),
+                                InGarage = reader.GetString(reader.GetOrdinal("InGarage")) == "1" ? "Yes" : "No",
+                                DealershipIdIbiza = reader.IsDBNull(reader.GetOrdinal("DealershipIdIbiza")) ? null : reader.GetString(reader.GetOrdinal("DealershipIdIbiza")),
+                                DealershipIdHawaii = reader.IsDBNull(reader.GetOrdinal("DealershipIdHawaii")) ? null : reader.GetString(reader.GetOrdinal("DealershipIdHawaii")),
+                                CanVehiclePaint = reader.GetString(reader.GetOrdinal("CanVehiclePaint")) == "1" ? "Yes" : "No",
+                                CanVehicleSticker = reader.GetString(reader.GetOrdinal("CanVehicleSticker")) == "1" ? "Yes" : "No",
+                                CanVehicleUpgrade = reader.GetString(reader.GetOrdinal("CanVehicleUpgrade")) == "1" ? "Yes" : "No",
+                                FrontTyres = reader.IsDBNull(reader.GetOrdinal("FrontTyres")) ? null : reader.GetString(reader.GetOrdinal("FrontTyres")),
+                                RearTyres = reader.IsDBNull(reader.GetOrdinal("RearTyres")) ? null : reader.GetString(reader.GetOrdinal("RearTyres")),
+                                WheelDrive = reader.IsDBNull(reader.GetOrdinal("WheelDrive")) ? null : reader.GetString(reader.GetOrdinal("WheelDrive")),
+                                EnginePosition = reader.IsDBNull(reader.GetOrdinal("EnginePosition")) ? null : reader.GetString(reader.GetOrdinal("EnginePosition")),
+                                FrontBrakes = reader.IsDBNull(reader.GetOrdinal("FrontBrakes")) ? null : reader.GetString(reader.GetOrdinal("FrontBrakes")),
+                                RearBrakes = reader.IsDBNull(reader.GetOrdinal("RearBrakes")) ? null : reader.GetString(reader.GetOrdinal("RearBrakes")),
+                            };
+
+                            Vehicles.Clear();
+                            Vehicles.Add(vehicle);
+                        }
+                        else
+                        {
+                            // Handle case where no vehicle was found for the given ID
+                            Console.WriteLine("Vehicle not found.");
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
